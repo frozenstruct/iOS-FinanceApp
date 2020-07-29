e535bb93114c74faecc86d0f1f1df1dd924e729e
Dimka@Home <deemka@home.ahaha.h>
2020-07-29 23:36:24 +0300

    Reconstrained FOVC bottom block

--- iOS-FinanceApp/Finance Overview/FOViewController.swift
+++ iOS-FinanceApp/Finance Overview/FOViewController.swift
@@ -19,6 +19,10 @@ class FOViewController: UIViewController {
     
     @IBOutlet weak var topStackView: UIStackView!
     
+    @IBOutlet weak var breakdownLabel: UILabel!
+    
+    @IBOutlet weak var addEntryButton: UIButton!
+    
     var data: [Dictionary<String, Int>.Element] {
         get { return DataManager.mapCategories(from: entries) }
     }
@@ -118,6 +122,9 @@ class FOViewController: UIViewController {
         currentBalanceLabel.translatesAutoresizingMaskIntoConstraints = false
         currentBalanceTitle.translatesAutoresizingMaskIntoConstraints = false
         topStackView.translatesAutoresizingMaskIntoConstraints = false
+        breakdownLabel.translatesAutoresizingMaskIntoConstraints = false
+        pivotTableView.translatesAutoresizingMaskIntoConstraints = false
+        addEntryButton.translatesAutoresizingMaskIntoConstraints = false
         
         currentBalanceTitle.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
         currentBalanceTitle.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 40).isActive = true
@@ -127,6 +134,18 @@ class FOViewController: UIViewController {
         
         topStackView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
         topStackView.topAnchor.constraint(equalTo: currentBalanceLabel.bottomAnchor, constant: 30).isActive = true
+        
+        breakdownLabel.topAnchor.constraint(equalTo: topStackView.bottomAnchor, constant: 50).isActive = true
+        breakdownLabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 31).isActive = true
+        
+        pivotTableView.topAnchor.constraint(equalTo: breakdownLabel.bottomAnchor, constant: 5).isActive = true
+        pivotTableView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 18).isActive = true
+        pivotTableView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -40).isActive = true
+        pivotTableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -50).isActive = true
+        
+        addEntryButton.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor).isActive = true
+        addEntryButton.topAnchor.constraint(equalTo: pivotTableView.bottomAnchor, constant: 3).isActive = true
+        addEntryButton.titleLabel?.resizeToText()
     }
     
 }
