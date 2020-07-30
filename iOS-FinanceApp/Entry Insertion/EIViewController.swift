23946b06f55a0adf8263bfbe128a3cd410caa88c
Dimka@Home <deemka@home.ahaha.h>
2020-07-30 21:58:27 +0300

    Reconstrained EIVC

--- iOS-FinanceApp/Entry Insertion/EIViewController.swift
+++ iOS-FinanceApp/Entry Insertion/EIViewController.swift
@@ -143,6 +143,7 @@ class EIViewController: UIViewController {
     // MARK: - Lifecycle Methods
     override func viewDidLoad() {
         super.viewDidLoad()
+        constrain()
         hideKeyboardWhenTappedAround()
         createDatePicker()
         createDatePickerToolbar()
@@ -200,9 +201,106 @@ class EIViewController: UIViewController {
             target: self,
             action: nil)
         
-        toolBar.setItems([todayButton, flexibleSpace, doneButton], animated: true)
+        
+        toolBar.setItems(
+            [todayButton, flexibleSpace, doneButton],
+            animated: true)
+        
         toolBar.barStyle = .default
+        
         toolBar.isTranslucent = true
+        
         toolBar.sizeToFit()
+    }
+    
+    func constrain() {
+        let safeArea = view.safeAreaLayoutGuide
+        
+        callToActionLabel
+            .translatesAutoresizingMaskIntoConstraints = false
+        
+        imageForCategory
+            .translatesAutoresizingMaskIntoConstraints = false
+        
+        topStackView
+            .translatesAutoresizingMaskIntoConstraints = false
+        
+        typeControl
+            .translatesAutoresizingMaskIntoConstraints = false
+        
+        addEntryButton
+            .translatesAutoresizingMaskIntoConstraints = false
+        
+        addCategoryButton
+            .translatesAutoresizingMaskIntoConstraints = false
+        
+        
+        callToActionLabel.topAnchor.constraint(
+            equalTo: safeArea.topAnchor,
+            constant: 25)
+            .isActive = true
+        
+        callToActionLabel.leadingAnchor.constraint(
+            equalTo: safeArea.leadingAnchor,
+            constant: 20)
+            .isActive = true
+        
+        
+        topStackView.topAnchor.constraint(
+            equalTo: callToActionLabel.bottomAnchor,
+            constant: 10)
+            .isActive = true
+        
+        topStackView.leadingAnchor.constraint(
+            equalTo: safeArea.leadingAnchor,
+            constant: 20).isActive = true
+        
+        topStackView.widthAnchor.constraint(
+            equalToConstant: 170)
+            .isActive = true
+        
+        
+        addCategoryButton.leadingAnchor.constraint(
+            equalTo: topStackView.trailingAnchor,
+            constant: 7)
+            .isActive = true
+        
+        addCategoryButton.bottomAnchor.constraint(
+            equalTo: topStackView.bottomAnchor,
+            constant: -41)
+            .isActive = true
+        
+        
+        addEntryButton.centerXAnchor.constraint(
+            equalTo: safeArea.centerXAnchor)
+            .isActive = true
+        
+        addEntryButton.centerYAnchor.constraint(
+            equalTo: safeArea.centerYAnchor)
+            .isActive = true
+        
+        addEntryButton.titleLabel?.resizeToText()
+        
+        
+        imageForCategory.topAnchor.constraint(
+            equalTo: topStackView.topAnchor)
+            .isActive = true
+        
+        imageForCategory.trailingAnchor.constraint(
+            equalTo: safeArea.trailingAnchor,
+            constant: -30)
+            .isActive = true
+        
+        
+        categoryIconPlaceholderLabel.centerXAnchor.constraint(
+            equalTo: imageForCategory.centerXAnchor)
+            .isActive = true
+        
+        categoryIconPlaceholderLabel.topAnchor.constraint(
+            equalTo: imageForCategory.bottomAnchor,
+            constant: 5)
+            .isActive = true
+        
+        categoryIconPlaceholderLabel.resizeToText()
     }
 }
